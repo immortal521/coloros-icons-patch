ee3b6459ece511403aa4742cabf45300f0793aad
immortal521 <immort521@gmail.com>
2026-03-21 13:54:37 +0800

    fix: create directories using variable paths
    - Use variable values for UXICONS_DST and OPLUS_DST in mkdir commands

--- module/customize.sh
+++ module/customize.sh
@@ -17,8 +17,8 @@ RUNTIME_DIR="$PERSIST_DIR/runtime"
 UXICONS_DST="my_product/media/theme/uxicons"
 OPLUS_DST="data/oplus/uxicons"
 
-mkdir -p "$MODPATH/UXICONS_DST"
-mkdir -p "$MODPATH/OPLUS_DST"
+mkdir -p "$MODPATH/$UXICONS_DST"
+mkdir -p "$MODPATH/$OPLUS_DST"
 
 set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm_recursive $BIN_DIR 0 0 0755 0755
